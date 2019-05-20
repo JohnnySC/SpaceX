29cde716d6915bec3a39db1dd50221c0b4224073
Асатрян Оганнес <OSAsatryan.SBT@sberbank.ru>
2019-05-20 23:05:28 +0300

    implemented delay for user input

--- app/src/main/java/com/github/johnnysc/spacex/presentation/MainScreenViewModel.kt
+++ app/src/main/java/com/github/johnnysc/spacex/presentation/MainScreenViewModel.kt
@@ -8,38 +8,54 @@ import androidx.lifecycle.viewModelScope
 import com.github.johnnysc.spacex.App
 import com.github.johnnysc.spacex.R
 import com.github.johnnysc.spacex.domain.Status
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 /**
  * @author Asatryan on 18.05.19
  */
 class MainScreenViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val interactor = (application as App).getDI().getLaunchesInteractor()
-
     val searchState = MutableLiveData<Int>()
     val progressState = MutableLiveData<Boolean>()
     val errorState = MutableLiveData<Int>()
 
+    private val interactor = (application as App).getDI().getLaunchesInteractor()
+    private var job: Job? = null
+
     fun fetch(query: String?) {
-        val inputDataValid = interactor.isInputDataValid(query)
-        if (inputDataValid == true) {
-            progressState.value = true
-            viewModelScope.launch {
+        viewModelScope.debounceLaunch(300) {
+            val inputDataValid = interactor.isInputDataValid(query)
+            if (inputDataValid == true) {
+                progressState.postValue(true)
                 when (interactor.fetch(query!!)) {
                     Status.NO_RESULTS -> showScreenWithId(R.id.no_results)
                     Status.NO_CONNECTION -> showScreenWithId(R.id.no_connection)
                     Status.SERVICE_UNAVAILABLE -> showScreenWithId(R.id.service_unavailable)
                     Status.SUCCESS -> showScreenWithId(R.id.go_to_search_results)
                 }
+            } else if (inputDataValid == false) {
+                errorState.postValue(R.string.invalid_input_message)
             }
-        } else if (inputDataValid == false) {
-            errorState.value = R.string.invalid_input_message
         }
     }
 
     private fun showScreenWithId(@IdRes id: Int) {
         progressState.postValue(false)
         searchState.postValue(id)
+    }
+
+    private fun CoroutineScope.debounceLaunch(
+        time: Long,
+        block: suspend CoroutineScope.() -> Unit
+    ): Job {
+        job?.cancel()
+        return launch() {
+            delay(time)
+            block()
+        }.also {
+            job = it
+        }
     }
 }
